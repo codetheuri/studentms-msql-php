b6dc9170684bd671cfdf9f1f415a060c50e5e2d7
codetheuri <theurij113@gmail.com>
2025-02-16 15:16:40 +0300

    updates

---
+++ database.php
@@ -0,0 +1,18 @@
+<?php
+// session_start();
+// if (!isset($_SESSION)) {
+//     session_start();
+// }
+
+$servername = "localhost";
+$username = "root";
+$password = "toor";
+$dbname = "schoolnew";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
