db0d4356457a8f5c66a93d42e477702f973eda2f
codetheuri <theurij113@gmail.com>
2025-02-19 14:49:39 +0300

    updates

--- login.php
+++ login.php
@@ -1,10 +1,10 @@
 <?php
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+// ini_set('display_errors', 1);
+// ini_set('display_startup_errors', 1);
+// error_reporting(E_ALL);
 
 
-session_start();
+// session_start();
 include_once 'database.php';
 
 ?>
